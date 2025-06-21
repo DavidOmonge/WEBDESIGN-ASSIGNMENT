47ff430b27d98be35a1e63b2c6cdf761270a4892
DavidOmonge <david.omonge@strathmore.edu>
2025-06-21 20:38:59 +0300

    Add files via upload

---
+++ CSS LAB 2/index.js
@@ -0,0 +1,7 @@
+document.getElementById('demo').innerHTML = "LP/52";
+
+const price  = 6;
+const item = 5;
+let total = price * item;
+
+// document.getElementById('demo-total').innerHTML = "Total: " + total;
